914fed2924ab1b5194a7c7263d88f2f4374003f0
DJakou herve <172897@umons.ac.be>
2019-05-20 21:53:54 +0200

    first commit

---
+++ app/src/main/java/com/example/projet_appmobile/ajout_de_resolution.java
@@ -0,0 +1,13 @@
+package com.example.projet_appmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ajout_de_resolution extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_de_resolution);
+    }
+}
